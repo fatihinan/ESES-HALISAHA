3e83aba9b1ecc8488f07b3374ff28254b12857f9
Fatih İnan <fatihinan37@hotmail.com>
2014-07-21 16:06:50 +0300

    ayrintılar sayfası oluşturuldu.

--- ESES HALISAHA/gen/com/example/eseshalisaha/R.java
+++ ESES HALISAHA/gen/com/example/eseshalisaha/R.java
@@ -35,10 +35,16 @@ public final class R {
         public static final int sportmen=0x7f02000a;
     }
     public static final class id {
-        public static final int action_settings=0x7f070003;
-        public static final int isim=0x7f070002;
+        public static final int action_settings=0x7f070009;
+        public static final int isim=0x7f070008;
+        public static final int iw_logo=0x7f070005;
         public static final int liste=0x7f070000;
-        public static final int simge=0x7f070001;
+        public static final int map=0x7f070006;
+        public static final int simge=0x7f070007;
+        public static final int tw_adres=0x7f070002;
+        public static final int tw_saha_adi=0x7f070001;
+        public static final int tw_sahalar=0x7f070004;
+        public static final int tw_telefon_numarasi=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
---
+++ ESES HALISAHA/src/com/example/eseshalisaha/Ayrintilar.java
@@ -0,0 +1,18 @@
+package com.example.eseshalisaha;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Ayrintilar extends Activity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ayrintilar);
+		
+		
+	}
+
+}
